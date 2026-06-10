f499fef84b88f1db07cc36261c9a6733840afe82
mai anh <vumaianh2006tbkx@gmail.com>
2026-06-10 08:19:23 +0700

    [FEATURE] Add the interface

--- hello-react/src/App.jsx
+++ hello-react/src/App.jsx
@@ -9,112 +9,69 @@ function App() {
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+      <div>
+        <header className="container header mb-2">
+            <h1 className="text-white">Quản lý danh sách sản phẩm</h1>
+            <p className="text-white">Giao diện nhập liệu và hiển thị danh sách sản phẩm một cách trực quan, rõ ràng</p>
+        </header>
+        <div className="d-flex">
+            <div className="addProductForm bg-white d-flex flex-column me-3 rounded-3">
+                <div className="d-flex flex-column mb-2 mt-2 p-2">
+                    <h3>Thêm sản phẩm mới</h3>
+                    <p className="text-secondary">Nhập đầy đủ thông tin để thêm sản phẩm vào danh sách</p>
+                </div>
+                <div className="d-flex flex-column mb-2 mt-2 p-2">
+                    <label htmlFor="productName">Tên sản phẩm</label>
+                    <input type="text" name="productName" id="productName" placeholder="Nhập tên sản phẩm"/>
 
-      <div className="ticks"></div>
+                    <label htmlFor="category">Danh mục</label>
+                    <select name="category" id="category">
+                        <option value="Sách">Sách</option>
+                        <option value="Áo">Áo</option>
+                        <option value="Quần">Quần</option>
+                    </select>
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+                    <label htmlFor="price">Giá</label>
+                    <input type="text" name="price" id="price" placeholder="Nhập giá"/>
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+                    <label htmlFor="state">Trạng thái còn hàng</label>
+                    <select name="state" id="state">
+                        <option value="Còn hàng">Còn hàng</option>
+                        <option value="Hết hàng">Hết hàng</option>
+                    </select>
+                </div>
+                <div className="d-flex mb-2 mt-2 gap-2 p-2">
+                    <button className="btn btn-primary">Thêm sản phẩm</button>
+                    <button className="btn btn-outline-secondary">Làm mới form</button>
+                </div>
+            </div>
+            <div className="listProduct d-flex flex-column mb-2 bg-white rounded-3 ">
+                <div className="p-2">
+                    <h3>Danh sách sản phẩm</h3>
+                    <p>Danh sách sản phẩm mẫu được hiển thi từ dữ liệu hẹ thống</p>
+                </div>
+                <table border="1">
+                    <thead>
+                        <td>STT</td>
+                        <td>TÊN SẢN PHẨM</td>
+                        <td>DANH MỤC</td>
+                        <td>GIÁ</td>
+                        <td>TRẠNG THÁI</td>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>STT</td>
+                            <td>Tên sản phẩm</td>
+                            <td>Danh mục</td>
+                            <td>Giá</td>
+                            <td>Trạng thái</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        
+    </div>
     </>
   )
 }
